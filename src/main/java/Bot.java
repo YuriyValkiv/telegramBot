214eff71fd121552d5a3de1509d69869209098db
YuriyValkiv <yurivalkiv@gmail.com>
2020-07-17 17:25:25 +0200

    Add files via upload

---
+++ src/main/java/Bot.java
@@ -0,0 +1,40 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+  public void onUpdateReceived(Update update) {
+
+    String message = update.getMessage().getText();
+    long chatId = update.getMessage().getChatId();
+    if (message.equalsIgnoreCase("Привіт")) {
+      sendMsg(chatId, "Привіт, Надю");
+    } else {
+      sendMsg(chatId, "Юра любить Надю!");
+    }
+
+  }
+
+  private synchronized void sendMsg(long chatId, String s) {
+    SendMessage sendMessage = new SendMessage();
+    sendMessage.enableMarkdown(true);
+    sendMessage.setChatId(chatId);
+    sendMessage.setText(s);
+    try {
+      execute(sendMessage);
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getBotUsername() {
+    return "YuraSpeakBot";
+  }
+
+  @Override
+  public String getBotToken() {
+    return "1315655773:AAEybENErU8KUDz5-ssbtgJ0XNoiWGgIAos";
+  }
+}
